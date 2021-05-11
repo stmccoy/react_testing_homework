2aa9eba54bfc8dcb6ba572db7bcbc9ecaec71a55
crispymccoy145 <samspam19941@gmail.com>
2021-05-11 17:31:47 +0100

    only one final test to go

--- react_calculator/cypress/integration/App.spec.js
+++ react_calculator/cypress/integration/App.spec.js
@@ -7,4 +7,66 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should have numbers that update the display', () => {
+    cy.get('#number4').click();
+    cy.get('.display').should('contain', '4')
+  })
+
+  it('should have arithmetical operations that update the display correctly', () => {
+    cy.get('#number4').click();
+    cy.get('#operator_add').click();
+    cy.get('#number3').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '7')
+  })
+
+  it('should be able to chain arithmetical operations together and update the display correctly', () => {
+    cy.get('#number4').click();
+    cy.get('#operator_add').click();
+    cy.get('#number3').click();
+    cy.get('#operator_equals').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number7').click();
+    cy.get('#operator_equals').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number1').click();
+    cy.get('.display').should('contain', '1')
+  })
+
+  it('should be able to process large numbers', () => {
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#operator_add').click();
+    cy.get('#number1').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '1000000000000')
+  })
+  it('should be able to process negative numbers', () => {
+    cy.get('#number1').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number9').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '-8')
+  })
+  it('should be able to process decimal numbers', () => {
+    cy.get('#number1').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '0.5')
+  })
 })
